93bffc16e6c2fa4c5d45ad7726926599b6f71ba4
root <root@master-node>
2022-02-19 12:03:52 +0000

    added nginx for frontend, script to start deployment

---
+++ start_wordpress.sh
@@ -0,0 +1,12 @@
+echo "[Setting up the environment]"
+source .env
+echo "[Pulling docker images]"
+sudo docker-compose pull
+echo "[Starting up DB]"
+sudo docker-compose up -d database
+sleep 20
+echo "[Starting wordpress with Nginx]"
+sudo docker-compose up -d wordpress nginx
+echo "Done!"
+echo "WordPress should be available at http://localhost"
+echo "use curl -L http://localhost"
